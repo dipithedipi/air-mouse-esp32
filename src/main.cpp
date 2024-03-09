e634f9ae91dcc2660df398be0063d2ec10a42fb1
dipi <diploma.francesco.studente@itispaleocapa.it>
2024-03-09 23:26:55 +0100

    add scroll Y

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,8 @@
 uint8_t data[6];
 int16_t gyroX, gyroZ;
 
-int Sensitivity = 600;
+int SensitivityX = 1200;
+int SensitivityY = 700;
 int delayi = 20;
 
 BleMouse bleMouse;
@@ -22,34 +23,41 @@ uint8_t i2cData[14];
 
 uint32_t leftButtonPressTime = 0;
 bool leftButtonHeld = false;
+bool bothButtonsPressed = false;
 
 const uint8_t IMUAddress = 0x68;
 const uint16_t I2C_TIMEOUT = 1000;
 
-uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop) {
+uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop)
+{
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
   Wire.write(data, length);
   return Wire.endTransmission(sendStop); // Returns 0 on success
 }
 
-uint8_t i2cWrite2(uint8_t registerAddress, uint8_t data, bool sendStop) {
+uint8_t i2cWrite2(uint8_t registerAddress, uint8_t data, bool sendStop)
+{
   return i2cWrite(registerAddress, &data, 1, sendStop); // Returns 0 on success
 }
 
-uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
+uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes)
+{
   uint32_t timeOutTimer;
   Wire.beginTransmission(IMUAddress);
   Wire.write(registerAddress);
   if (Wire.endTransmission(false))
     return 1;
   Wire.requestFrom(IMUAddress, nbytes, (uint8_t) true);
-  for (uint8_t i = 0; i < nbytes; i++) {
+  for (uint8_t i = 0; i < nbytes; i++)
+  {
     if (Wire.available())
       data[i] = Wire.read();
-    else {
+    else
+    {
       timeOutTimer = micros();
-      while (((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
+      while (((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available())
+        ;
       if (Wire.available())
         data[i] = Wire.read();
       else
@@ -59,7 +67,8 @@ uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
   return 0;
 }
 
-void setup() {
+void setup()
+{
   Wire.begin(I2C_SDA, I2C_SCL);
   pinMode(L_BTN_PIN, INPUT);
   pinMode(R_BTN_PIN, INPUT);
@@ -68,10 +77,14 @@ void setup() {
   i2cData[1] = 0x00;
   i2cData[3] = 0x00;
 
-  while (i2cWrite(0x19, i2cData, 4, false));
-  while (i2cWrite2(0x6B, 0x01, true));
-  while (i2cRead(0x75, i2cData, 1));
-  while (i2cRead(0x3B, i2cData, 6));
+  while (i2cWrite(0x19, i2cData, 4, false))
+    ;
+  while (i2cWrite2(0x6B, 0x01, true))
+    ;
+  while (i2cRead(0x75, i2cData, 1))
+    ;
+  while (i2cRead(0x3B, i2cData, 6))
+    ;
 
   timer = micros();
   Serial.begin(115200);
@@ -79,36 +92,74 @@ void setup() {
   delay(100);
 }
 
-void loop() {
- while (i2cRead(0x3B, i2cData, 14));
- gyroX = ((i2cData[8] << 8) | i2cData[9]);
- gyroZ = ((i2cData[12] << 8) | i2cData[13]);
+void loop()
+{
+  while (i2cRead(0x3B, i2cData, 14))
+    ;
+  gyroX = ((i2cData[8] << 8) | i2cData[9]);
+  gyroZ = ((i2cData[12] << 8) | i2cData[13]);
 
- gyroX = gyroX / Sensitivity / 1.1 * -1;
- gyroZ = gyroZ / Sensitivity * -1;
-
- if (bleMouse.isConnected()) {
+  gyroX = gyroX / SensitivityX / 1.1 * -1;
+  gyroZ = gyroZ / SensitivityY * -1;
+  if (bleMouse.isConnected())
+  {
     // Gestione del tasto sinistro
-    if (digitalRead(L_BTN_PIN) == HIGH) {
-      if (!leftButtonHeld) {
+    if (digitalRead(L_BTN_PIN) == HIGH && bothButtonsPressed == false)
+    {
+      if (!leftButtonHeld)
+      {
         leftButtonPressTime = micros();
         leftButtonHeld = true;
         bleMouse.press(MOUSE_LEFT); // Invia il comando di pressione
       }
-    } else if (leftButtonHeld) {
-      if (micros() - leftButtonPressTime > 200000) { // 0.2 secondi in microsecondi
+    }
+    else if (leftButtonHeld)
+    {
+      if (micros() - leftButtonPressTime > 200000)
+      { // 0.2 secondi in microsecondi
         Serial.println("Left click long press");
         bleMouse.press(MOUSE_LEFT);
-      } else {
+      }
+      else
+      {
         Serial.println("Left click");
-        bleMouse.click(MOUSE_LEFT);
       }
       bleMouse.release(MOUSE_LEFT); // Invia il comando di rilascio
       leftButtonHeld = false;
     }
 
-    if (digitalRead(R_BTN_PIN) == HIGH) {
-      delay(100);
+    // Gestione del tasto destro
+    if (digitalRead(R_BTN_PIN) == HIGH)
+    {
+      if (leftButtonHeld)
+      { // Assicurati che il tasto sinistro non sia premuto
+        bothButtonsPressed = true;
+      }
+    }
+    else
+    {
+      bothButtonsPressed = false;
+    }
+
+    if (bothButtonsPressed)
+    {
+      // Esegui l'azione di scorrimento in base alla direzione del giroscopio
+      if (gyroZ < 0)
+      {
+        Serial.println("Scroll up");
+        bleMouse.move(0, 0, 1);
+      }
+      else if (gyroZ > 0)
+      {
+        Serial.println("Scroll down");
+        bleMouse.move(0, 0, -1);
+      }
+      // Aggiungi qui altre direzioni se necessario
+    }
+
+    if (digitalRead(R_BTN_PIN) == HIGH && bothButtonsPressed == false)
+    {
+      delay(150);
       Serial.println("Right click");
       bleMouse.click(MOUSE_RIGHT);
     }
@@ -117,7 +168,8 @@ void loop() {
     Serial.print(" ");
     Serial.print(gyroZ);
     Serial.print("\r\n");
-    bleMouse.move(gyroZ, -gyroX);
+    if (bothButtonsPressed == false)
+      bleMouse.move(gyroZ, -gyroX);
     delay(delayi);
- }
+  }
 }
